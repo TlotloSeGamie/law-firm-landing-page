c69ee74c2eca2b356b510776a1acb4f36e2d8681
TlotloSeGamie <t88segamie@gmail.com>
2025-01-31 04:33:17 +0200

    pricing css

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,61 +1,65 @@
 import React from "react";
-import './Pricing.css';
-
+import "./Pricing.css";
 
 const Pricing = () => {
-    return(
+    return (
         <div className="pricing-main">
             <div className="pricing-container">
                 <div className="pricing-top">
                     <h5>Pricing</h5>
-                    <h2><span>Simole</span> and <span>Affordable</span> Pricing Pans</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do</p>
+                    <h2>
+                        <span>Simple</span> and <span>Affordable</span> Pricing Plans
+                    </h2>
+                    <p>Choose a plan that fits your needs and budget.</p>
                 </div>
                 <div className="prices">
-                    <div className="basic">
+                    <div className="pricing-card basic">
                         <h5>Basic</h5>
                         <div className="per">
-                            <h2>R499</h2><h4>/month</h4>
+                            <h2>R499</h2>
+                            <h4>/month</h4>
                         </div>
                         <div className="price-content">
                             <ul>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
+                                <li> Essential features</li>
+                                <li> Limited support</li>
+                                <li> Single user access</li>
+                                <li> Basic analytics</li>
                             </ul>
                         </div>
-                        <button className="sign-up">sign up</button>
+                        <button className="sign-up">Sign Up</button>
                     </div>
-                    <div className="professional">
+                    <div className="pricing-card professional">
                         <h5>Professional</h5>
                         <div className="per">
-                            <h2>R999</h2><h4>/month</h4>
+                            <h2>R999</h2>
+                            <h4>/month</h4>
                         </div>
                         <div className="price-content">
                             <ul>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
+                                <li> Advanced features</li>
+                                <li> Priority support</li>
+                                <li> Multiple users</li>
+                                <li> Detailed reports</li>
                             </ul>
                         </div>
-                        <button className="sign-up">sign up</button>
+                        <button className="sign-up">Sign Up</button>
                     </div>
-                    <div className="enterprise">
+                    <div className="pricing-card enterprise">
                         <h5>Enterprise</h5>
                         <div className="per">
-                            <h2>R2, 499</h2><h4>/month</h4>
+                            <h2>R2,499</h2>
+                            <h4>/month</h4>
                         </div>
                         <div className="price-content">
                             <ul>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
-                                <li>Lorem ipsum dolor sit amet</li>
+                                <li> All professional features</li>
+                                <li> Dedicated support</li>
+                                <li> Unlimited users</li>
+                                <li> Custom analytics</li>
                             </ul>
                         </div>
-                        <button className="sign-up">sign up</button>
+                        <button className="sign-up">Sign Up</button>
                     </div>
                 </div>
             </div>
